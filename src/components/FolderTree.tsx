db2d962bcaacc1c6aaf9ffd0465ec1437fcf7866
janjanpower <janlee0701@gmail.com>
2025-09-18 19:11:28 +0800

    0918-6

--- src/components/FolderTree.tsx
+++ src/components/FolderTree.tsx
@@ -195,6 +195,7 @@ const handleUploadClick = (e: React.MouseEvent) => {
               onFileUpload={onFileUpload}
               onFolderCreate={onFolderCreate}
               onDelete={onDelete}
+              onPreview={onPreview}
             />
           ))}
         </div>
